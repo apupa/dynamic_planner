a037797e4e370e90a3741ec077f12a5c152b1255
Italo-99 <italoalmirante.10@gmail.com>
2025-01-18 15:41:58 +0100

    Debugging8

--- src/dynamic_planner.cpp
+++ src/dynamic_planner.cpp
@@ -813,7 +813,7 @@ void DynamicPlanner::moveRobot(const moveit_msgs::RobotTrajectory& robot_traject
   sensor_msgs::JointState trajectory_pose;
   // Fill the name of the joints
   trajectory_pose.name = robot_trajectory.joint_trajectory.joint_names;
-  ROS_INFO(trajectory_pose.name);
+  ROS_INFO(trajectory_pose.name[5]);
   // Setup the rate of the planner execution
   // Hypothesis: all the points of the trajectory are uniformely sampled in time; if not, thery are forced here
   ros::Rate traj_exec_rate(1/(robot_trajectory.joint_trajectory.points[1].time_from_start.toSec()));
@@ -833,8 +833,8 @@ void DynamicPlanner::moveRobot(const moveit_msgs::RobotTrajectory& robot_traject
         trajectory_pose.velocity[k] = 0.;
       }
 
-        ROS_INFO(trajectory_pose.position);
-        ROS_INFO(trajectory_pose.velocity);
+        ROS_INFO(trajectory_pose.position[5]);
+        ROS_INFO(trajectory_pose.velocity[5]);
 
 
       moveRobot(trajectory_pose);
